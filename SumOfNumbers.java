2dbc6fb9736d4bfe1e71ea2808436eb0d39f5801
RG-5557 <54259667+RG-5557@users.noreply.github.com>
2019-09-03 17:01:48 +0530

    Added sumOfNumbersUsingIndex() method
    Skip all numbers after 6 till the index of last 9 and add remaining numbers

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -7,21 +7,24 @@ public class SumOfNumbers {
 
 	public static void main(String[] args) {
 
-		
-		// sum of number from list 
+		// sum of number from list
 		// condition: skip all numbers between 6 and 9 including both numbers
 		List<Integer> list = Arrays.asList(new Integer[] { 4, 5, 6, 7, 8, 9, 1, 10 });
-		System.out.println("Sum of numbers :: " + sumOfNumbersFromList(list));
-		
-		
+		System.out.println("Sum of numbers(while loop) :: " + sumOfNumbersUsingWhile(list));
+
 		List<Integer> list1 = Arrays.asList(new Integer[] { 4, 5, 6, 7, 6, 8, 12, 9, 10, 9, 11, 10 });
-		System.out.println("Sum of numbers :: " + sumOfNumbersFromList(list1));
+		System.out.println("Sum of numbers(while loop) :: " + sumOfNumbersUsingWhile(list1));
+
+		List<Integer> list2 = Arrays.asList(new Integer[] { 9, 1, 6, 10 });
+		System.out.println("Sum of numbers(using index) :: " + sumOfNumbersUsingIndex(list2));
+
+		List<Integer> list3 = Arrays
+				.asList(new Integer[] { 9, 1, 6, 10, 9, 90 });
+		System.out.println("Sum of numbers(using index) :: " + sumOfNumbersUsingIndex(list3));
 
 	}
-	
-	
 
-	public static int sumOfNumbersFromList(List<Integer> numbers) {
+	public static int sumOfNumbersUsingWhile(List<Integer> numbers) {
 
 		int sum = 0;
 		boolean add = true;
@@ -30,15 +33,15 @@ public class SumOfNumbers {
 		for (int i = 0; i < numbers.size(); i++) {
 
 			int num = numbers.get(i);
-			
+
 			while (add) {
 				// if num != 6 add to existing sum
 				if (num != 6) {
 					sum += num;
 					break;
 				} else {
-					// set add false if num == 9
-					add = false;					
+					// set add false if num == 6
+					add = false;
 				}
 			}
 
@@ -48,13 +51,38 @@ public class SumOfNumbers {
 					break;
 				} else {
 					// set add false if num == last 9 from list
-				if(i == lastNinesIndex){
-					add = true;
-				}
-				break;
+					if (i == lastNinesIndex) {
+						add = true;
+					}
+					break;
 				}
 			}
 		}
 		return sum;
 	}
+
+	public static int sumOfNumbersUsingIndex(List<Integer> numbers) {
+
+		int sum = 0;
+		int lastNinesIndex = numbers.lastIndexOf(9);
+
+		for (int i = 0; i < numbers.size(); i++) {
+
+			int num = numbers.get(i);
+
+			if (num != 6) {
+				sum += num;
+			} else {
+				// if last index of 9 > i set 9's index to i
+				if (lastNinesIndex > i) {
+					i = lastNinesIndex;
+				} else {
+					// if last index of 9 not > i break the loop
+					break;
+				}
+			}
+		}
+		return sum;
+	}
+
 }
