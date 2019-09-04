a4b5f9093acb473fd5bd4372db049222b51427d2
RG-5557 <54259667+RG-5557@users.noreply.github.com>
2019-09-04 16:38:48 +0530

    Created InterfaceMain class
    Simple program for Inner interface demo

---
+++ InterfaceMain.java
@@ -0,0 +1,41 @@
+package com.demo.programs.intrfc;
+
+interface A {
+	public abstract String print(String name);
+}
+
+interface B {
+	public abstract String print(String name);
+
+	static void method() {
+		System.out.println("Test");
+	}
+}
+
+class Inf implements InterfaceMain.A {
+
+	@Override
+	public void a() {
+		System.out.println("Inner class interface is called");
+
+	}
+}
+
+public class InterfaceMain {
+
+	public static void main(String[] args) {
+
+		B b = str -> { return str.concat(" is string"); };
+		System.out.println(b.print("abc"));
+		B.method();
+
+		InterfaceMain.A ia = () -> System.out.println("Inner interface is called");
+		ia.a();
+
+	}
+
+	interface A {
+		void a();
+	}
+
+}
