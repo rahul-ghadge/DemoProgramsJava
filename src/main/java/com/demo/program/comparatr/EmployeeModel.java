a0c1127ed1c6412277c974bcd4271571021f3217
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-09-23 15:01:25 +0530

    Create EmployeeModel.java
    EmployeeModel class with equals() and hashcode() method as Key to TreeMap

---
+++ src/main/java/com/demo/program/comparatr/EmployeeModel.java
@@ -0,0 +1,97 @@
+package com.demo.program.comparatr;
+
+public class EmployeeModel {
+
+	private int id;
+	private String name;
+	private String department;
+	private float salary;
+
+	public EmployeeModel() {
+	}
+
+	public EmployeeModel(int id, String name, String department, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((department == null) ? 0 : department.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + Float.floatToIntBits(salary);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeeModel other = (EmployeeModel) obj;
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		} else if (!department.equals(other.department))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Float.floatToIntBits(salary) != Float.floatToIntBits(other.salary))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "\nEmployeeModel [id=" + id + ", name=" + name + ", department="
+				+ department + ", salary=" + salary + "]";
+	}
+
+}
