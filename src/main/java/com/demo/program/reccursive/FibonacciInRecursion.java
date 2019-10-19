25a97b561496c84ac363ee627089d96370e1a96b
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-10-19 13:06:51 +0530

    Fabonacci number at given position using recursion

---
+++ src/main/java/com/demo/program/reccursive/FibonacciInRecursion.java
@@ -0,0 +1,19 @@
+package com.demo.program.reccursive;
+
+public class FibonacciInRecursion {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num + "'th Fabonacci No : " + fibonacci(num));
+	}
+	
+	/*
+	 * Calculate fabonacci at index
+	 */
+	static int fibonacci(int n) {
+		if (n <= 1)
+			return n;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
