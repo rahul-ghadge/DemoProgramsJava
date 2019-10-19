5bdb50e1e92ae7c6aef5d5200274db0a577b1d54
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-10-19 12:59:55 +0530

    Added different approaches in factorials recursion

--- src/main/java/com/demo/program/reccursive/FactorialReccursive.java
+++ src/main/java/com/demo/program/reccursive/FactorialReccursive.java
@@ -4,6 +4,8 @@ public class FactorialReccursive {
 
     public static void main(String[] args) {
         System.out.println(fact(4));
+        System.out.println(factOptimized(5));
+        System.out.println(factorial(4));
     }
 
 
@@ -11,6 +13,14 @@ public class FactorialReccursive {
         if (n == 0)
             return 1;
         else
-            return (n * fact(n - 1));
+            return (n * fact(n - 1)); // tail recursion
     }
+    
+    public static int factOptimized(int n) {
+		return n == 1 ? n  : n * fact(n - 1); // tail recurrsion
+	}
+	
+	public static int factorial(int n, int a) {
+		return n == 0 ? a : factorial(n - 1, n * a);
+	}
 }
