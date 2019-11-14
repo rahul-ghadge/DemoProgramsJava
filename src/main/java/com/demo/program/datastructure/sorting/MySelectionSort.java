4595a4327f57d93dba58e7a40ac17ea106525117
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-11-14 16:07:04 +0530

    Updated comments for insertion sort

--- src/main/java/com/demo/program/datastructure/sorting/MySelectionSort.java
+++ src/main/java/com/demo/program/datastructure/sorting/MySelectionSort.java
@@ -7,18 +7,19 @@ public class MySelectionSort {
     public static void doSelectionSort(int[] arr) {
 
         for (int i = 0; i < arr.length - 1; i++) {
-            int index = i;
+            int index = i;          // Consider current elements index is min value in array
 
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[index] > arr[j])
-                    index = j;
+                    index = j;      // Minimum values index in array
             }
 
+            // swap min element with current index
             int smallerNumber = arr[index];
             arr[index] = arr[i];
             arr[i] = smallerNumber;
 
-
+            
             System.out.println(Arrays.toString(arr));
 
         }
