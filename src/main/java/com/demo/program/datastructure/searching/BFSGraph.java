54ddfae769d07fd1ef8b31211a5a2804c281fe02
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-11-20 15:21:17 +0530

    BFS code optimized

--- src/main/java/com/demo/program/datastructure/searching/BFSGraph.java
+++ src/main/java/com/demo/program/datastructure/searching/BFSGraph.java
@@ -52,16 +52,16 @@ public class BFSGraph {
 		Node node50 = new Node(50);
 		Node node70 = new Node(70);
 
-		node40.addneighbours(node10);	// directed edge form 40 to 10
-		node40.addneighbours(node20);	// directed edge form 40 to 20
-		node10.addneighbours(node30);	// directed edge form 10 to 30
-		node20.addneighbours(node10);	// directed edge form 20 to 10
-		node20.addneighbours(node30);	// directed edge form 20 to 30
-		node20.addneighbours(node60);	// directed edge form 20 to 60
-		node20.addneighbours(node50);	// directed edge form 20 to 50
-		node30.addneighbours(node60);	// directed edge form 30 to 60
-		node60.addneighbours(node70);	// directed edge form 60 to 70
-		node50.addneighbours(node70);	// directed edge form 50 to 70
+		node40.addNeighbour(node10);	// directed edge form 40 to 10
+		node40.addNeighbour(node20);	// directed edge form 40 to 20
+		node10.addNeighbour(node30);	// directed edge form 10 to 30
+		node20.addNeighbour(node10);	// directed edge form 20 to 10
+		node20.addNeighbour(node30);	// directed edge form 20 to 30
+		node20.addNeighbour(node60);	// directed edge form 20 to 60
+		node20.addNeighbour(node50);	// directed edge form 20 to 50
+		node30.addNeighbour(node60);	// directed edge form 30 to 60
+		node60.addNeighbour(node70);	// directed edge form 60 to 70
+		node50.addNeighbour(node70);	// directed edge form 50 to 70
 		
 		System.out.println("\nThe BFS traversal of the graph is :");
 		BFSGraph bfsGraph = new BFSGraph();
