3e4a67f547e4cdc4ffbb16fb0b3c21935480efd4
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2020-03-11 14:05:32 +0530

    Update GetClassInfo.java

--- src/main/java/com/demo/program/reflection/GetClassInfo.java
+++ src/main/java/com/demo/program/reflection/GetClassInfo.java
@@ -5,15 +5,21 @@ public class GetClassInfo {
 	public static void main(String[] args) {
 
 		Class<GetClassInfo> cls = GetClassInfo.class;
+		
 		System.out.println("Class :: " + cls);
+		System.out.println("Class Name :: " + cls.getName());
+		
 		
 		System.out.println("--------------------------------------------------------- ");
+		
 		
 		try {
 			@SuppressWarnings("unchecked")
 			Class<GetClassInfo> clsNew = (Class<GetClassInfo>) Class
 						.forName("com.demo.program.reflection.GetClassInfo");
+			
 			System.out.println("Class New :: " + clsNew);
+			System.out.println("Class Name :: " + clsNew.getName());
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -26,5 +32,7 @@ public class GetClassInfo {
 // Output 
 // ---------------------------------------------------------------
 // Class :: class com.demo.program.reflection.GetClassInfo
+// Class Name :: com.demo.program.reflection.GetClassInfo
 // --------------------------------------------------------- 
 // Class New :: class com.demo.program.reflection.GetClassInfo
+// Class Name :: com.demo.program.reflection.GetClassInfo
